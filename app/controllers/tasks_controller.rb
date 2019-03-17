82863d0185da54f2c83062e6ca8d0cd827d0d0cf
redfield920 <atsuki.k6296@gmail.com>
2019-03-17 15:08:02 +0900

    task new&edit page create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,25 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(content: params[:content])
+    if @task.save
+      flash[:notice] = "タスクを登録しました"
+      redirect_to("/")
+    end
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    @task.content = Task.update(content: params[:content])
+    if @task.save
+       flash[:notice] = "タスクを編集しました"
+       redirect_to("/tasks")
+    end
+  end
 end
